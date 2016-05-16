a8a7425f562f54aec8b520e54db70a11bc3385bd
oommiiz <chidchanok.wungsri@hotmail.com>
2016-05-16 13:33:32 +0700

    SignUp Button Intent to SignUPClass Finish

--- app/src/main/java/wungsri/chidchanok/snrurun/MainActivity.java
+++ app/src/main/java/wungsri/chidchanok/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package wungsri.chidchanok.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }//End of Main Method เมธอด คือสิ่งที่ห่อหุ้ม statement
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+    }//End of Method คลิกปุ่ม SignUpMain หน้า Main แล้วให้เปิดหน้า SignUP
+
+} //End of Main Class
